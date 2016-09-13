84f72defd8071242a8a61edb31605718133e3a55
DrBenCarson <adrianoz@comcast.net>
2016-09-13 11:31:55 -0600

    Added

---
+++ RadiusOfCircle/src/RadiusOfCircles.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+public class RadiusOfCircles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner circleReader=new Scanner(System.in);
+		System.out.println("What is the area?_ ");
+		double a=circleReader.nextDouble();
+		double area=(Math.sqrt(a/Math.PI));
+		System.out.println("The radius of you circle is: " + area);
+
+	}
+
+
+	}
+
+
